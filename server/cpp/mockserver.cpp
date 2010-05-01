cd692bce76ba7107673c49d31dd33568f8766da1
ddk <arena431@gmail.com>
2010-05-01 14:41:39 +0900

    mockserver

--- server/cpp/mockserver.cpp
+++ server/cpp/mockserver.cpp
@@ -1,19 +1,12 @@
-/*
-  Copyright 2005-2007 Adobe Systems Incorporated
-
-  Use, modification and distribution are subject to the Boost Software License,
-  Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
-  http://www.boost.org/LICENSE_1_0.txt).
-
-  See http://opensource.adobe.com/gil for most recent version including documentation.
+/* 
+ * Copyright 2005-2007 Adobe Systems Incorporated
+ *
+ *  Use, modification and distribution are subject to the Boost Software License,
+ * Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+ * http://www.boost.org/LICENSE_1_0.txt).
+ *
+ * See http://opensource.adobe.com/gil for most recent version including documentation.
 */
-
-/*************************************************************************************************/
-
-/// \file
-/// \brief Test file for convolve_rows() and convolve_cols() in the numeric extension
-/// \author Lubomir Bourdev and Hailin Jin
-/// \date February 27, 2007
 
 #include <boost/gil/image.hpp>
 #include <boost/gil/typedefs.hpp>
@@ -21,9 +14,10 @@
 
 using namespace boost::gil;
 
-// Models a Unary Function
-template <typename P>   // Models PixelValueConcept
-struct mandelbrot_fn {
+/* Models a Unary Function */
+template <typename P>   /* Models PixelValueConcept */
+struct mandelbrot_fn
+{  
   typedef point2<ptrdiff_t>    point_t;
 
   typedef mandelbrot_fn        const_t;
@@ -33,28 +27,33 @@ struct mandelbrot_fn {
   typedef point_t              argument_type;
   typedef reference            result_type;
   BOOST_STATIC_CONSTANT(bool, is_mutable=false);
-
+  
   value_type                    _in_color,_out_color;
   point_t                       _img_size;
-  static const int MAX_ITER=100;        // max number of iterations
+  static const int MAX_ITER = 100;  /* max number of iterations */ 
 
   mandelbrot_fn() {}
-  mandelbrot_fn(const point_t& sz, const value_type& in_color, const value_type& out_color) : _in_color(in_color), _out_color(out_color), _img_size(sz) {}
+  mandelbrot_fn(const point_t& sz, const value_type& in_color, const value_type& out_color) :
+	_in_color(in_color), _out_color(out_color), _img_size(sz){} 
 
-  result_type operator()(const point_t& p) const {
+  result_type operator()(const point_t& p) const
+  {	
 	// normalize the coords to (-2..1, -1.5..1.5)
 	// (actually make y -1.0..2 so it is asymmetric, so we can verify some view factory methods)
-	double t=get_num_iter(point2<double>(p.x/(double)_img_size.x*3-2, p.y/(double)_img_size.y*3-1.0f));//1.5f));
+	double t = get_num_iter(point2<double>(p.x/(double)_img_size.x*3-2, p.y/(double)_img_size.y*3-1.0f));//1.5f));	
 	t=pow(t,0.2);
 
-	value_type ret;
-	for (int k=0; k<num_channels<P>::value; ++k)
-	  ret[k]=(typename channel_type<P>::type)(_in_color[k]*t + _out_color[k]*(1-t));
+	value_type ret;	
+	for (int k = 0 ; k<num_channels<P>::value ; ++k) {	  
+	  ret[k] = (typename channel_type<P>::type)(_in_color[k]*t + _out_color[k]*(1-t));
+	}
+	
 	return ret;
   }
 
 private:
-  double get_num_iter(const point2<double>& p) const {
+  double get_num_iter(const point2<double>& p) const
+  {	
 	point2<double> Z(0,0);
 	for (int i=0; i<MAX_ITER; ++i) {
 	  Z = point2<double>(Z.x*Z.x - Z.y*Z.y + p.x, 2*Z.x*Z.y + p.y);
